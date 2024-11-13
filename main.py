0cdd19396ccbefdac87c9a65f5db9f857cdabfdd
calebj07 <148258995+calebj07@users.noreply.github.com>
2024-11-13 10:02:33 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -16,6 +16,64 @@ def b(x: int, y: int, spacing: int):
     OLED12864_I2C.pixel(x + 2, y + 6, 1)
     return 4 + spacing  # width of letter
 
+def c(x: int, y: int, spacing: int):
+    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
+    OLED12864_I2C.vline(x, y + 3, 3, 1)
+    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
+    return 3 + spacing # width of letter
+
+def d(x: int, y: int, spacing: int):
+    OLED12864_I2C.vline(x + 3, y, 7, 1)
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.pixel(x + 2, y + 3, 1)
+    OLED12864_I2C.vline(x, y + 3, 3, 1)
+    OLED12864_I2C.pixel(x + 1, y + 6, 1)
+    OLED12864_I2C.pixel(x + 2, y + 5, 1)
+    return 4 + spacing  # width of letter
+
+def e(x: int, y: int, spacing: int):
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.pixel(x, y + 3, 1)
+    OLED12864_I2C.pixel(x + 2, y + 3, 1)
+    OLED12864_I2C.hline(x, y + 4, 3, 1)
+    OLED12864_I2C.pixel(x, y + 5, 1)
+    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
+    return 3 + spacing  # width of letter
+
+def f(x: int, y: int, spacing: int):
+    OLED12864_I2C.pixel(x + 2, y, 1)
+    OLED12864_I2C.pixel(x + 1, y + 1, 1)
+    OLED12864_I2C.pixel(x + 3, y + 1, 1)
+    OLED12864_I2C.vline(x + 1, y + 2, 5, 1)
+    OLED12864_I2C.hline(x + 0, y + 3, 3, 1)
+    return 3 + spacing  # width of letter
+
+def g(x: int, y: int, spacing: int):
+    OLED12864_I2C.pixel(x + 1, y + 3, 1)
+    OLED12864_I2C.vline(x, y + 4, 2, 1)
+    OLED12864_I2C.vline(x + 2, y + 4, 5, 1)
+    OLED12864_I2C.pixel(x + 1, y + 6, 1)
+    OLED12864_I2C.hline(x, y + 9, 2, 1)
+    return 3 + spacing  # width of letter
+
+def h(x: int, y: int, spacing: int):
+    OLED12864_I2C.vline(x, y, 7, 1)
+    OLED12864_I2C.pixel(x + 1, y + 3, 1)
+    OLED12864_I2C.vline(x + 2, y + 4, 3, 1)
+    return 3 + spacing  # width of letter
+
+def i(x: int, y: int, spacing: int):
+    OLED12864_I2C.pixel(x + 0, y + 2, 1)
+    OLED12864_I2C.vline(x, y + 4, 3, 1)
+    return 1 + spacing # width of letter
+
+def j(x: int, y: int, spacing: int):
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.vline(x + 1, y + 4, 5, 1)
+    OLED12864_I2C.pixel(x - 1, y + 8, 1)
+    OLED12864_I2C.pixel(x, y + 9, 1)
+    return 1 + spacing  # width of letter
+
 def k(x: int, y: int, spacing: int):
     OLED12864_I2C.vline(x, y, 7, 1)
     OLED12864_I2C.pixel(x + 3, y + 2, 1)
@@ -25,17 +83,6 @@ def k(x: int, y: int, spacing: int):
     OLED12864_I2C.pixel(x + 3, y + 6, 1)
     return 4 + spacing  # width of letter
 
-def c(x: int, y: int, spacing: int):
-    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
-    OLED12864_I2C.vline(x, y + 3, 3, 1)
-    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
-    return 3 + spacing # width of letter
-
-def i(x: int, y: int, spacing: int):
-    OLED12864_I2C.pixel(x + 0, y + 2, 1)
-    OLED12864_I2C.vline(x, y + 4, 3, 1)
-    return 1 + spacing # width of letter
-
 def s(x: int, y: int, spacing: int):
     OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
     OLED12864_I2C.pixel(x + 0, y + 3, 1)
@@ -44,29 +91,8 @@ def s(x: int, y: int, spacing: int):
     OLED12864_I2C.hline(x, y + 6, 2, 1)
     return 3 + spacing  # width of letter
 
-def d(x: int, y: int, spacing: int):
-    OLED12864_I2C.vline(x, y, 7, 1)
-    OLED12864_I2C.pixel(x + 1, y + 2, 1)
-    OLED12864_I2C.pixel(x + 2, y + 3, 1)
-    OLED12864_I2C.pixel(x + 2, y + 4, 1)
-    OLED12864_I2C.pixel(x + 1, y + 5, 1)
-    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
-    return 4 + spacing  # width of letter
-
-def e(x: int, y: int, spacing: int):
-    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
-    OLED12864_I2C.vline(x, y + 3, 3, 1)
-    OLED12864_I2C.hline(x + 1, y + 4, 2, 1)
-    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
-    return 3 + spacing  # width of letter
-
-def f(x: int, y: int, spacing: int):
-    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
-    OLED12864_I2C.vline(x, y + 3, 7, 1)
-    OLED12864_I2C.hline(x + 1, y + 4, 2, 1)
-    return 3 + spacing  # width of letter
 def space():
-    return 2
+    return 1
 
 # Starting position
 x = 1  # starting x position
@@ -89,8 +115,14 @@ def draw_text(text: str, x: int, y: int, spacing: int):
             x += e(x, y, spacing)
         elif char == 'f':
             x += f(x, y, spacing)
+        elif char == 'g':
+            x += g(x, y, spacing)
+        elif char == 'h':
+            x += h(x, y, spacing)
         elif char == 'i':
             x += i(x, y, spacing)
+        elif char == 'j':
+            x += j(x, y, spacing)
         elif char == 'k':
             x += k(x, y, spacing)
         elif char == 's':
@@ -99,4 +131,4 @@ def draw_text(text: str, x: int, y: int, spacing: int):
             x += space()
 
 # Example usage
-draw_text("sick ab def", x, y, spacing)
+draw_text("sick ab defghij", x, y, spacing)
--- main.ts
+++ main.ts
@@ -20,17 +20,6 @@ function b(x: number, y: number, spacing: number) {
 }
 
 //  width of letter
-function k(x: number, y: number, spacing: number) {
-    OLED12864_I2C.vline(x, y, 7, 1)
-    OLED12864_I2C.pixel(x + 3, y + 2, 1)
-    OLED12864_I2C.pixel(x + 2, y + 3, 1)
-    OLED12864_I2C.pixel(x + 1, y + 4, 1)
-    OLED12864_I2C.pixel(x + 2, y + 5, 1)
-    OLED12864_I2C.pixel(x + 3, y + 6, 1)
-    return 4 + spacing
-}
-
-//  width of letter
 function c(x: number, y: number, spacing: number) {
     OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
     OLED12864_I2C.vline(x, y + 3, 3, 1)
@@ -39,10 +28,80 @@ function c(x: number, y: number, spacing: number) {
 }
 
 //  width of letter
+function d(x: number, y: number, spacing: number) {
+    OLED12864_I2C.vline(x + 3, y, 7, 1)
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.pixel(x + 2, y + 3, 1)
+    OLED12864_I2C.vline(x, y + 3, 3, 1)
+    OLED12864_I2C.pixel(x + 1, y + 6, 1)
+    OLED12864_I2C.pixel(x + 2, y + 5, 1)
+    return 4 + spacing
+}
+
+//  width of letter
+function e(x: number, y: number, spacing: number) {
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.pixel(x, y + 3, 1)
+    OLED12864_I2C.pixel(x + 2, y + 3, 1)
+    OLED12864_I2C.hline(x, y + 4, 3, 1)
+    OLED12864_I2C.pixel(x, y + 5, 1)
+    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
+    return 3 + spacing
+}
+
+//  width of letter
+function f(x: number, y: number, spacing: number) {
+    OLED12864_I2C.pixel(x + 2, y, 1)
+    OLED12864_I2C.pixel(x + 1, y + 1, 1)
+    OLED12864_I2C.pixel(x + 3, y + 1, 1)
+    OLED12864_I2C.vline(x + 1, y + 2, 5, 1)
+    OLED12864_I2C.hline(x + 0, y + 3, 3, 1)
+    return 3 + spacing
+}
+
+//  width of letter
+function g(x: number, y: number, spacing: number) {
+    OLED12864_I2C.pixel(x + 1, y + 3, 1)
+    OLED12864_I2C.vline(x, y + 4, 2, 1)
+    OLED12864_I2C.vline(x + 2, y + 4, 5, 1)
+    OLED12864_I2C.pixel(x + 1, y + 6, 1)
+    OLED12864_I2C.hline(x, y + 9, 2, 1)
+    return 3 + spacing
+}
+
+//  width of letter
+function h(x: number, y: number, spacing: number) {
+    OLED12864_I2C.vline(x, y, 7, 1)
+    OLED12864_I2C.pixel(x + 1, y + 3, 1)
+    OLED12864_I2C.vline(x + 2, y + 4, 3, 1)
+    return 3 + spacing
+}
+
+//  width of letter
 function i(x: number, y: number, spacing: number) {
     OLED12864_I2C.pixel(x + 0, y + 2, 1)
     OLED12864_I2C.vline(x, y + 4, 3, 1)
     return 1 + spacing
+}
+
+//  width of letter
+function j(x: number, y: number, spacing: number) {
+    OLED12864_I2C.pixel(x + 1, y + 2, 1)
+    OLED12864_I2C.vline(x + 1, y + 4, 5, 1)
+    OLED12864_I2C.pixel(x - 1, y + 8, 1)
+    OLED12864_I2C.pixel(x, y + 9, 1)
+    return 1 + spacing
+}
+
+//  width of letter
+function k(x: number, y: number, spacing: number) {
+    OLED12864_I2C.vline(x, y, 7, 1)
+    OLED12864_I2C.pixel(x + 3, y + 2, 1)
+    OLED12864_I2C.pixel(x + 2, y + 3, 1)
+    OLED12864_I2C.pixel(x + 1, y + 4, 1)
+    OLED12864_I2C.pixel(x + 2, y + 5, 1)
+    OLED12864_I2C.pixel(x + 3, y + 6, 1)
+    return 4 + spacing
 }
 
 //  width of letter
@@ -56,36 +115,8 @@ function s(x: number, y: number, spacing: number) {
 }
 
 //  width of letter
-function d(x: number, y: number, spacing: number) {
-    OLED12864_I2C.vline(x, y, 7, 1)
-    OLED12864_I2C.pixel(x + 1, y + 2, 1)
-    OLED12864_I2C.pixel(x + 2, y + 3, 1)
-    OLED12864_I2C.pixel(x + 2, y + 4, 1)
-    OLED12864_I2C.pixel(x + 1, y + 5, 1)
-    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
-    return 4 + spacing
-}
-
-//  width of letter
-function e(x: number, y: number, spacing: number) {
-    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
-    OLED12864_I2C.vline(x, y + 3, 3, 1)
-    OLED12864_I2C.hline(x + 1, y + 4, 2, 1)
-    OLED12864_I2C.hline(x + 1, y + 6, 2, 1)
-    return 3 + spacing
-}
-
-//  width of letter
-function f(x: number, y: number, spacing: number) {
-    OLED12864_I2C.hline(x + 1, y + 2, 2, 1)
-    OLED12864_I2C.vline(x, y + 3, 7, 1)
-    OLED12864_I2C.hline(x + 1, y + 4, 2, 1)
-    return 3 + spacing
-}
-
-//  width of letter
 function space(): number {
-    return 2
+    return 1
 }
 
 //  Starting position
@@ -111,8 +142,14 @@ function draw_text(text: string, x: number, y: number, spacing: number) {
             x += e(x, y, spacing)
         } else if (char == "f") {
             x += f(x, y, spacing)
+        } else if (char == "g") {
+            x += g(x, y, spacing)
+        } else if (char == "h") {
+            x += h(x, y, spacing)
         } else if (char == "i") {
             x += i(x, y, spacing)
+        } else if (char == "j") {
+            x += j(x, y, spacing)
         } else if (char == "k") {
             x += k(x, y, spacing)
         } else if (char == "s") {
@@ -125,4 +162,4 @@ function draw_text(text: string, x: number, y: number, spacing: number) {
 }
 
 //  Example usage
-draw_text("sick ab def", x, y, spacing)
+draw_text("sick ab defghij", x, y, spacing)
